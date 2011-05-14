d9ecff31cb95bf4a4fd3844fd41d10f29a8fcd92
Eclarian <joel.kallman@gmail.com>
2011-05-14 00:16:01 -0400

    Updating the Format_DB to accept and format Objects.  Also updating to convert string representations of booleans and null to actual types.

--- config/format_config.php
+++ config/format_config.php
@@ -12,6 +12,36 @@
 $config['default_format_config'] = 'format_general';
 
 
+/*
+|--------------------------------------------------------------------------
+| Enable Psuedo Bool Conversion
+|--------------------------------------------------------------------------
+|
+| This allows you to pass true, false, null as strings in your configuration
+| which will be parsed and interpreted as booleans to the functions they
+| are passed to
+|
+| @since 1.0.2
+*/
+
+$config['enable_psuedo_bool_conversion'] = TRUE;
+
+
+/*
+|--------------------------------------------------------------------------
+| Parse Types Supported
+|--------------------------------------------------------------------------
+|
+| The Parse Types Supported by Default
+|
+| If you add more parsable strings, such as "array()" to array(),
+| you can add them below
+|
+| @since 1.0.2
+*/
+
+$config['parse_types_supported'] = array('true' => TRUE, 'false' => FALSE, 'null' => NULL);
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +50,12 @@ $config['default_format_config'] = 'format_general';
 |
 | Allows you to specify where specific methods are located.  Procedural functions (helpers)
 | should NOT be included in the list below.  Only methods that occur within a class.
-| This allows you to take full advantage of the CI singleton by accessing any publically  
+| This allows you to take full advantage of the CI singleton by accessing any publicly 
 | accessible method from ANY class loaded at the time of this libraries method call.
 |
 | NOTE: The array format should be array('METHOD_NAME' => 'CLASS_NAME');
 */
 
 $config['method_map'] = array(
-	'convert_sql_date' => 'utilities',
-	// ADD YOURS HERE !! 
+	'clean_for_json' => 'utilities', // @example	Method 'clean_for_json' in Class 'utilities'
 );
--- libraries/Format_db.php
+++ libraries/Format_db.php
@@ -2,44 +2,60 @@
 /**
  *	Format Database
  *
- *	This library allows you to target format your entire database
+ *	This library allows you to target format your entire database (or any array)
  *	for the specific display scenario you find yourself in (e.g. table)
  *
  *	USAGE EXAMPLES
  *	  + BASIC USAGE
  *		$query = $this->db->get();
- *		$this->format_db->run($query); // Query is now formatted!
+ *		$result = $query->result();
+ *		$this->format_db->run($result); // Query is now formatted!
  *
  *	  + MULTIPLE FORMAT -> Allows you to have a default format and then supplement it.
  *		$query = $this->db->get();
- *		$this->format_db->run($query); // Query is now formatted!
- *		$this->format_db->config('new_config', TRUE)->run($query); // Query run through new Format!
+ *		$result = $query->result();
+ *		$this->format_db->run($result);
+ *		$this->format_db->config('new_config', TRUE)->run($result); 
  *		
  *
  *	@author		Eclarian Dev Team (Joel Kallman & Joseph Moss)
- *	@copyright	Copyright (c) 2009-2011, Eclarian LLC
+ *	@copyright	Copyright (c) 2011, Eclarian LLC
  *	@license	MIT
  *	@link		http://www.eclarian.com/codeigniter-format-database-library/
- *	@version 	1.0
+ *	@version 	1.0.2
  */
 class Format_db {
 
 	/**
+	 *	CodeIgniter Object
+	 */
+	private $ci;
+
+	/**
 	 *	Format Configuration for Fields
 	 */
-	private $config = array();
-	private $config_set = FALSE;
+	private $config							= array();
+	private $config_set						= FALSE;
+	private $disallow_formatting			= FALSE;
 	
 	/**
 	 *	Format DB Configuration
 	 */
-	private $default_format_config = 'format_general';
-	private $method_map = array();
+	private $default_format_config			= 'format_general';
+	private $enable_psuedo_bool_conversion	= TRUE;	// @since 1.0.2
+	private $method_map						= array();
 	
 	/**
-	 *	New Value allows for Proper Chaining
+	 *	Allows for Proper Function Chaining
 	 */
-	private $new_val = '';
+	private $new_val						= '';
+	
+	/**
+	 *	Parse to Types Supported Conversions
+	 */
+	private $parse_types_supported			= array('true' => TRUE, 'false' => FALSE, 'null' => NULL);
+	
+	// ----------------------------------------------------------------------------------------
 	
 	/**
 	 *	Constructor
@@ -48,9 +64,9 @@ class Format_db {
 	 */
 	public function __construct()
 	{
-		$CI =& get_instance();
-		$CI->config->load('format_config', TRUE);
-		$config = $CI->config->item('format_config');
+		$this->ci = get_instance();
+		$this->ci->config->load('format_config', TRUE);
+		$config = $this->ci->config->item('format_config');
 		
 		foreach($config as $prop => $val)
 		{
@@ -58,20 +74,47 @@ class Format_db {
 		}
 	}
 	
+	// ----------------------------------------------------------------------------------------
+	
 	/**
 	 *	Reset Config
 	 *
 	 *	Clears Configuration
 	 *	
+	 *	@param	bool	$disallow_formatting	No Formatting Performed for Subsequent Run()
 	 *	@return	object
 	 */
-	public function reset_config()
+	public function reset_config($disallow_formatting = FALSE)
 	{
-		unset($this->config);
+		$this->disallow_formatting = $disallow_formatting;
+		// This solves a potential bug in certain versions of PHP that don't reset the array with just a redeclaration.		
+		unset($this->config);	
 		$this->config = array();
 		return $this;
 	}
 	
+	// ----------------------------------------------------------------------------------------
+	
+	/**
+	 *	Enable Formatting
+	 *	
+	 *	This allows you to re-enable formatting after it has 
+	 *	been disabled with a second configuration.
+	 *	
+	 *	This can be useful if you are formatting your DB result multiple times
+	 *	and one of your formatting methods runs a DB query which when run with
+	 *	the current formatting causes an fatal error because of the infinite 
+	 *	recursion which results in the PHP memory limit being reached.  
+	 *	
+	 *	@return object
+	 */
+	public function enable_formatting()
+	{
+		$this->disallow_formatting = FALSE;
+		return $this;
+	}
+	
+	// ----------------------------------------------------------------------------------------
 	
 	/**
 	 *	Config
@@ -88,53 +131,107 @@ class Format_db {
 		if( ! $this->config_set OR $reset)
 		{			
 			// Load Specified Configuration for Formatting
-			$CI =& get_instance();
 			$config = ($config !== '') ? $config: $this->default_format_config;
-			$CI->config->load($config, TRUE, TRUE); // Suppressing Errors if no File Included
-			$this->config = $CI->config->item($config);
+			$this->ci->config->load($config, TRUE, TRUE); // Suppressing Errors if no File Included
+			$this->config = $this->ci->config->item($config);
 			$this->config_set = TRUE;
 		}
 		
 		return $this;
 	}
 	
+	// ----------------------------------------------------------------------------------------
 	
 	/**
 	 *	Run
 	 *
 	 *	Formats the Results based on the configuration setup
 	 *
-	 *	@param	array	Can be a Result Array or Row Array
-	 *	@param	string	Specify a Different Configuration
+	 *	Accepts Arrays (e.g. row_array() and result_array()) 
+	 *	and Objects since 1.0.2 (e.g. row() and result())
+	 *
+	 *	@param	mixed
 	 *	@return	object
 	 */
-	public function run(&$array)
+	public function run(&$mixed)
 	{
-		$CI =& get_instance();	
-		$this->config(); // Load Default Configuration if None Loaded Yet
-		
-		// Format the Data
-		if(is_array($array) && ! empty($array))
+		if($this->disallow_formatting === TRUE) 
 		{
-			foreach($array as $k => &$row)
+			return $this->enable_formatting();
+		}
+		
+		$this->config(); // Load Default Configuration if None Loaded Yet
+				
+		// Row or Result Array OR possible Result Object
+		if(is_array($mixed))
+		{
+			foreach($mixed as $k => $row)
 			{
-				if(is_array($row))
+				if(is_object($row))
 				{
-					foreach($row as $field => &$val)
-					{
-						$array[$k][$field] = $this->filter($field, $val, $CI);
-					}
+					$mixed[$k] = $this->_object_loop($row);
 				}
 				else
 				{
-					$array[$k] = $this->filter($k, $row, $CI);
+					$mixed[$k] = $this->_array_loop($row, $k);
 				}
-			}
+			}			
 		}
-		
+		// Row Object
+		elseif(is_object($mixed))
+		{
+			$mixed = $this->_object_loop($mixed);
+		}
+			
 		return $this;
 	}
 	
+	// ----------------------------------------------------------------------------------------	
+	
+	/**
+	 *	Array Loop
+	 *
+	 *	@param	mixed	Array or String
+	 *	@param	string
+	 *	@return	array
+	 */
+	private function _array_loop($row, $k)
+	{
+		$array = array();		
+		if(is_array($row))
+		{
+			foreach($row as $field => &$val)
+			{
+				$array[$field] = $this->filter($field, $val);
+			}
+		}
+		else
+		{
+			$array = $this->filter($k, $row);
+		}
+		
+		return $array;
+	}
+	
+	// ----------------------------------------------------------------------------------------	
+	
+	/**
+	 *	Object Loop
+	 *
+	 *	@param	object
+	 *	@return	object
+	 */
+	private function _object_loop($row)
+	{
+		foreach(get_object_vars($row) as $field => $val)
+		{
+			$row->$field = $this->filter($field, $val);
+		}
+		
+		return $row;
+	}
+	
+	// ----------------------------------------------------------------------------------------	
 		
 	/**
 	 *	Filter
@@ -144,10 +241,9 @@ class Format_db {
 	 *	
 	 *	@param	string	
 	 *	@param	string	
-	 *	@param	object	
 	 *	@return	string
 	 */
-	private function filter($field, $value, $CI)
+	private function filter($field, $value)
 	{		
 		if( ! isset($this->config[$field]))
 		{
@@ -159,7 +255,7 @@ class Format_db {
 			if(is_object($this->config[$field]) && is_callable($this->config[$field]))
 			{
 				return $this->config[$field]($value);
-			}			
+			}		
 			
 			// Fetch Functions to Apply to the Value
 			$methods = explode('|', $this->config[$field]);
@@ -179,6 +275,14 @@ class Format_db {
 					// Allow for additional parameters separated by periods
 					$params = explode('.', $method);
 					$method = array_shift($params);
+					
+					// Parse the parameters to their corresponding types
+					if($this->enable_psuedo_bool_conversion === TRUE)
+					{
+						$params = $this->parse_to_type($params);
+					}					
+					
+					// Add the Value as the FIRST parameter of the function
 					array_unshift($params, $this->new_val);
 					
 					// Check if Function is a Registered Class Method
@@ -188,9 +292,9 @@ class Format_db {
 					}					
 					
 					// Run Class Method Formatting
-					if($class !== '' && method_exists($CI->$class, $method))
+					if($class !== '' && method_exists($this->ci->$class, $method))
 					{
-						$this->new_val = call_user_func_array(array($CI->$class, $method), $params);
+						$this->new_val = call_user_func_array(array($this->ci->$class, $method), $params);
 					}
 					// Run Procedural Function Formatting
 					elseif(function_exists($method))
@@ -202,11 +306,10 @@ class Format_db {
 						log_message('debug', "Class: $class | Function: $method -- Nothing was Called.  Format_db Library");
 					}
 					
-					// Handle Errors
+					// Handle Errors >> REFERENCE: http://php.net/manual/en/function.call-user-func-array.php
 					if($this->new_val === FALSE)
 					{
-						// Log Error - Function Didn't work for some reason
-						log_message('error', "Class: $class | Function: $method -- call_user_func() failed to work properly.  Format_db Library");
+						log_message('error', "Class: $class | Function: $method -- call_user_func_array() failed to work properly.  Format_db Library");
 						return $value;
 					}
 				}
@@ -216,7 +319,38 @@ class Format_db {
 		}		
 	}
 	
+	// ----------------------------------------------------------------------------------------
 	
+	/**
+	 *	Parse to Type
+	 *	
+	 *	Takes a string and parses it to boolean or null.
+	 *	NOTE: This is not evaluating the expression as to whether
+	 *	it is TRUE or FALSE, but rather checking for explicit 
+	 *	references to "true", "false", or "null".
+	 *
+	 *	THIS FUNCTION IS CASE SENSITIVE.  All references to must be lowercase
+	 *
+	 *	@since	1.0.2
+	 *	@param	array
+	 *	@return	array
+	 */
+	private function parse_to_type($params = array())
+	{
+		if(empty($params))
+		{
+			return $params;
+		}
+		
+		$new = array();		
+		foreach($params as $param)
+		{
+			$new[] = (isset($this->parse_types_supported[$param])) ? $this->parse_types_supported[$param]: $param;
+		}
+		
+		return $new;
+	}
+
 }
 
 /* End of file ./libraries/Format_db.php */
